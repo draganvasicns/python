6ae7b28e18e220cc70d2850793431e3f1467141d
draganvasicns <draganvasicns@gmail.com>
2018-12-28 07:04:34 +0100

    Initial db commit

---
+++ db/riskAccessLog.py
@@ -0,0 +1,39 @@
+import mysql.connector
+from mysql.connector import Error
+
+config = {
+  'user': 'webrisk',
+  'password': 'virgo',
+  'host': '10.1.2.25',
+  'database': 'web_risk',
+  'raise_on_warnings': True
+}
+
+selectStatement = '''SELECT * FROM user'''
+
+def openConnection():
+    """ Connect to my sql database """
+    print ('In connect')
+    conn = None
+    try:
+        conn = mysql.connector.connect(**config)
+        if conn.is_connected:
+            print("Connection is open")
+            return conn
+    except Error as error:
+        print(f'Something wrong happend {error}')
+    return None
+
+def getDataFromDb(cursor):
+    cursor.execute(selectStatement)
+    for row in cursor.fetchall():
+            print(row)
+
+def showResults():
+    conn = openConnection()
+    if conn != None:
+        getDataFromDb(conn.cursor())
+        conn.close()
+    print ('Example is over')
+
+if __name__== '__main__' : showResults()
